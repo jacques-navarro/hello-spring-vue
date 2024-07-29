fda04f46afe95d35a2178353e1aff40b4dceac89
jacques-navarro <4421229+jacques-navarro@users.noreply.github.com>
2024-07-29 11:05:30 +0200

    Initial commit

---
+++ src/test/java/dev/passingarguments/vue_hello/VueHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.passingarguments.vue_hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
